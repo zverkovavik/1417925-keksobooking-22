4cb83dc0a0f5d537ba037d506cbbf987965622cc
zverkovavik <viktorya_zverkova@mail.ru>
2021-02-28 13:18:29 +0300

    The form validation

--- js/form.js
+++ js/form.js
@@ -35,3 +35,91 @@ const goActive = () => {
 
 export { goActive };
 // подумать над оптимизацией
+
+const housingType = document.querySelector('#type');
+const apartmentPrice = document.querySelector('#price');
+
+housingType.addEventListener('change', () => {
+  switch (housingType.value) {
+    case 'bungalow':
+      apartmentPrice.setAttribute('min', '0');
+      apartmentPrice.setAttribute('placeholder', '0');
+      break;
+    case 'flat':
+      apartmentPrice.setAttribute('min', '1000');
+      apartmentPrice.setAttribute('placeholder', '1000');
+      break;
+    case 'house':
+      apartmentPrice.setAttribute('min', '5000');
+      apartmentPrice.setAttribute('placeholder', '5000');
+      break;
+    case 'palace':
+      apartmentPrice.setAttribute('min', '10000');
+      apartmentPrice.setAttribute('placeholder', '10000');
+      break;
+  }
+});
+const timeForm = document.querySelector('.ad-form__element--time');
+const checkInTime = document.querySelector('#timein');
+const checkOutTime = document.querySelector('#timeout');
+
+timeForm.addEventListener('change', (evt) => {
+  checkInTime.value = evt.target.value;
+  checkOutTime.value = evt.target.value;
+});
+
+
+const titleAd = document.querySelector('#title');
+titleAd.addEventListener('input', () => {
+  if (titleAd.validity.tooLong) {
+    titleAd.setCustomValidity('Длина заголовка объявления не более 100 символов.');
+  } else if (titleAd.validity.tooShort) {
+    titleAd.setCustomValidity('Длина заголовка объявления не менее 30 символов.')
+  } else if (titleAd.validity.valueMissing) {
+    titleAd.setCustomValidity('Это обязательное поле.')
+  } else {
+    titleAd.setCustomValidity('');
+  }
+  titleAd.reportValidity();
+});
+
+apartmentPrice.addEventListener('input', () => {
+  if (apartmentPrice.validity.valueMissing) {
+    apartmentPrice.setCustomValidity('Это обязательное поле.');
+  } else if (apartmentPrice.validity.rangeOverflow) {
+    apartmentPrice.setCustomValidity('Максимальная цена - 1 000 000.');
+  } else {
+    apartmentPrice.setCustomValidity('');
+  }
+  apartmentPrice.reportValidity();
+});
+
+const roomNumber = document.querySelector('#room_number');
+const guestsNumber = document.querySelector('#capacity');
+
+roomNumber.addEventListener('click', () => {
+  if (roomNumber.value === '1') {
+    for (let i = 0; i < guestsNumber.children.length; i++) {
+      guestsNumber.children[i].setAttribute('disabled', 'disabled');
+    }
+    guestsNumber.children[guestsNumber.children.length - 2].removeAttribute('disabled');
+    guestsNumber.children[guestsNumber.children.length - 2].setAttribute('selected', 'selected');
+  } else if (roomNumber.value === '100') {
+    for (let i = 0; i < guestsNumber.children.length; i++) {
+      guestsNumber.children[i].setAttribute('disabled', 'disabled');
+    }
+    guestsNumber.children[guestsNumber.children.length - 1].removeAttribute('disabled');
+    guestsNumber.children[guestsNumber.children.length - 1].setAttribute('selected', 'selected');
+  } else if (roomNumber.value === '2') {
+    for (let i = 1; i <= roomNumber.value; i++) {
+      guestsNumber.children[i].removeAttribute('disabled');
+    }
+    guestsNumber.children[0].setAttribute('disabled', 'disabled');
+    guestsNumber.children[guestsNumber.children.length - 1].setAttribute('disabled', 'disabled');
+  } else {
+    for (let i = 0; i < roomNumber.value; i++) {
+      guestsNumber.children[i].removeAttribute('disabled');
+    }
+    guestsNumber.children[guestsNumber.children.length - 1].setAttribute('disabled', 'disabled');
+  }
+});
